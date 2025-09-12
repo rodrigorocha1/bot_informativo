4084fc9050257a507946496153cb10bdb662d55c
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-09-11 22:46:36 -0300

    import List

--- src/core/observador.py
+++ src/core/observador.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
+from typing import List, Dict
 
 
 class Observador(ABC):
 
     @abstractmethod
-    def atualizar(self, dado):
+    def atualizar(self, dado: List[Dict]):
         pass
