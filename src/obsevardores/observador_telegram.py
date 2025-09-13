0cb5d9d9586d46df04db0c50f4cd969749ac36bb
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-09-13 12:16:31 -0300

    protegendo valores

--- src/obsevardores/observador_telegram.py
+++ src/obsevardores/observador_telegram.py
@@ -11,7 +11,7 @@ class ObservadorTelegram(Observador):
     def __init__(self):
         self.__TOKEN_TELEGRAM = Config.TOKEN_TELEGRAM
         self.__CHAT_ID = Config.CHAT_ID
-        self.__bot = TeleBot(self.__CHAT_ID)
+        self.__bot = TeleBot(self.__TOKEN_TELEGRAM)
 
     def atualizar(self, dado: List[Dict]):
         self.__bot.send_message(self.__CHAT_ID, dado, parse_mode="MarkdownV2")
